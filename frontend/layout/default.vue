d817ac2cacf86aed763e6ea775115a4e35cde70c
LeThanhTung0707 <tenanhlavip@gmail.com>
2023-03-08 14:27:42 +0700

    Frontend Init

---
+++ frontend/layout/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <header>
+      
+    </header>
+  </div>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
